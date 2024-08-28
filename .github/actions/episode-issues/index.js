b1842ef2fd4efd2c90ca2f64d144b5026eb201e0
NootSponge <github@nootsponge.com>
2024-08-28 23:47:25 +0000

    Test new workflow and action

---
+++ .github/actions/episode-issues/index.js
@@ -0,0 +1,25 @@
+import core from "@actions/core";
+import github from "@actions/github";
+import { context } from "@actions/github";
+
+export async function run() {
+  try {
+    const token = core.getInput("repo-token", { required: true });
+    const octokit = github.getOctokit(token);
+
+    const issue_number = context.payload.issue.number;
+    const { owner, repo } = context.repo;
+
+    const body = context.payload.comment.body.trim();
+
+    // for debugging
+    core.info(`Issue number: ${issue_number}`);
+    core.info(`Owner: ${owner}`);
+    core.info(`Repo: ${repo}`);
+    core.info(`Comment body: ${body}`);
+
+    return;
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
