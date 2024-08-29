76a3db5e8fb48069586463af176214369630194b
NootSponge <hello@nootsponge.com>
2024-08-28 20:33:49 -0500

    Need to get context

--- .github/actions/episode-issues/index.js
+++ .github/actions/episode-issues/index.js
@@ -11,6 +11,8 @@ import { context } from "@actions/github";
 
   const body = context.payload.issue.body.trim();
 
+  core.info(context);
+
   // this is about to be real ugly
 
   // detect command with regex
