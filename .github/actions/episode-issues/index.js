2c3af8e3ded42b5c7d3f748271b1b68413e5607b
NootSponge <hello@nootsponge.com>
2024-08-29 08:21:47 -0500

    double up backslash

--- .github/actions/episode-issues/index.js
+++ .github/actions/episode-issues/index.js
@@ -11,10 +11,10 @@ const { owner, repo } = context.repo;
 const body = context.payload.issue.body.trim();
 
 // test case
-const regex = /\\.([a-zA-Z]+)\\n```yaml\\n([\\S\\s]*?)\\n```/gm;
+// const regex = /\\.([a-zA-Z]+)\\n```yaml\\n([\\S\\s]*?)\\n```/gm;
 
 // Alternative syntax using RegExp constructor
-// const regex = new RegExp('\\.([a-zA-Z]+)\\n```yaml\\n([\\S\\s]*?)\\n```', 'gm')
+const regex = new RegExp("\\.([a-zA-Z]+)\\n```yaml\\n([\\S\\s]*?)\\n```", "gm");
 
 const str = `14111awdawdawdadw
   .createEpisode
