466de0832add9fdf5fe216dec49bde20e5096199
NootSponge <hello@nootsponge.com>
2024-08-29 13:37:05 -0500

    Create file with issue command and commit changes

--- .github/actions/episode-issues/index.js
+++ .github/actions/episode-issues/index.js
@@ -2,22 +2,55 @@ import core from "@actions/core";
 import github from "@actions/github";
 import { context } from "@actions/github";
 
-const token = core.getInput("github_token", { required: true });
-const octokit = github.getOctokit(token);
+(async () => {
+  const token = core.getInput("github_token", { required: true });
+  const octokit = github.getOctokit(token);
 
-const issue_number = context.payload.issue.number;
-const { owner, repo } = context.repo;
+  core.setOutput("changed", false);
 
-const body = context.payload.issue.body;
+  const issue_number = context.payload.issue.number;
+  const { owner, repo } = context.repo;
 
-// this is about to be real ugly
-// detect command with regex
-const regex = /\.([a-zA-Z]+)[\r\n]+```yaml[\r\n]+([\S\s]*?)[\r\n]+[\s]*?```/;
-const matches = body.match(regex);
+  const body = context.payload.issue.body;
 
-core.info(matches);
-if (!matches) {
-  core.info("❌ No commands found in issue body, gettin' outta here");
-}
+  // this is about to be real ugly
+  // detect command with regex
+  const regex =
+    /\.([a-zA-Z]+) (s[0-9]+e[0-9]+)[\r\n]+```yaml[\r\n]+([\S\s]*?)[\r\n]+[\s]*?```/;
+  const matches = body.match(regex);
 
-core.info("✅ Found commands in issue body, let's do this");
+  if (!matches) {
+    core.notice("❌ No commands found in issue body, gettin' outta here");
+  }
+
+  const command = matches[1].toLowerCase();
+  const identifier = matches[2];
+  const yaml = matches[3];
+  core.notice(`✅ Found command in issue body, command is ${command}`);
+
+  switch (command) {
+    case "createepisode":
+      core.notice(`🎉 Creating episode '${identifier}'`);
+      // create file "src/content/${identifier}.yaml" with yaml content
+      const fs = require("fs");
+      const path = require("path");
+
+      const contentPath = path.join("src", "content", `${identifier}.yaml`);
+      fs.writeFileSync(contentPath, yaml);
+
+      // reply to issue with success message
+      await octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number,
+        body: `✅ Recognized command, created episode '${identifier}'.`,
+      });
+      core.setOutput("changed", true);
+
+      core.notice("✅ Done creating episode");
+      break;
+
+    default:
+      core.notice("❌ Command not recognized, gettin' outta here");
+  }
+})();
