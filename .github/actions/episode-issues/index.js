89f07e3c6e1cda2e295fc4262b974d6ae6f62f4a
NootSponge <hello@nootsponge.com>
2024-08-29 08:30:07 -0500

    ugh

--- .github/actions/episode-issues/index.js
+++ .github/actions/episode-issues/index.js
@@ -11,10 +11,13 @@ const { owner, repo } = context.repo;
 const body = context.payload.issue.body.trim();
 
 // test case
-const regex = /\.([a-zA-Z]+)\n```yaml\n([\S\s]*?)\n[\s]*?```/gm;
+// const regex = /\.([a-zA-Z]+)\n```yaml\n([\S\s]*?)\n[\s]*?```/gm;
 
 // Alternative syntax using RegExp constructor
-// const regex = new RegExp("\\.([a-zA-Z]+)\\n```yaml\\n([\\S\\s]*?)\\n```", "gm");
+const regex = new RegExp(
+  "\\.([a-zA-Z]+)\\n```yaml\\n([\\S\\s]*?)\\n[\\s]*?```",
+  "gm",
+);
 
 const str = `14111awdawdawdadw
   .createEpisode
