44fb6510341f9332f423a7ac06130e8514428cb6
NootSponge <hello@nootsponge.com>
2024-08-29 08:27:44 -0500

    whitespace

--- .github/actions/episode-issues/index.js
+++ .github/actions/episode-issues/index.js
@@ -11,44 +11,44 @@ const { owner, repo } = context.repo;
 const body = context.payload.issue.body.trim();
 
 // test case
-// const regex = /\\.([a-zA-Z]+)\\n```yaml\\n([\\S\\s]*?)\\n```/gm;
+const regex = /\.([a-zA-Z]+)\n```yaml\n([\S\s]*?)\n[\s]*?```/gm;
 
 // Alternative syntax using RegExp constructor
 const regex = new RegExp("\\.([a-zA-Z]+)\\n```yaml\\n([\\S\\s]*?)\\n```", "gm");
 
 const str = `14111awdawdawdadw
-.createEpisode
-\`\`\`yaml
----
-season: 4
-episode: 60
-title: Dementia, Pineapple on Pizza, and Headless Lounge Love??
-description: In this episode, some returning guests and our resident blue wolf play some old and brand new games around the classic "Whose Line Is It Anyway?" format.
-format: whoseLine
-airDate: 2024-05-25
-duration: "02:03:17"
-ytID: 1qqVYa4d9Wg
-tags:
-  - game show
-crew:
-  - name: Wolf Seisenbacher
-    role: host
-  - name: GlitchFlux
-    role: cohost
-  - name: NootSponge
-    role: producer
-  - name: Matorin Talfax
-    role: cameraSwitcher
-  - name: RaspberryKitty1
-    role: cohost
-guests:
-  - Defhammer
-  - Namogod
-  - SlyTheFloof
-  - Cloud_striker
-  - Cerealbowl
-  - Beethoven
-\`\`\`
+  .createEpisode
+  \`\`\`yaml
+  ---
+  season: 4
+  episode: 60
+  title: Dementia, Pineapple on Pizza, and Headless Lounge Love??
+  description: In this episode, some returning guests and our resident blue wolf play some old and brand new games around the classic "Whose Line Is It Anyway?" format.
+  format: whoseLine
+  airDate: 2024-05-25
+  duration: "02:03:17"
+  ytID: 1qqVYa4d9Wg
+  tags:
+    - game show
+  crew:
+    - name: Wolf Seisenbacher
+      role: host
+    - name: GlitchFlux
+      role: cohost
+    - name: NootSponge
+      role: producer
+    - name: Matorin Talfax
+      role: cameraSwitcher
+    - name: RaspberryKitty1
+      role: cohost
+  guests:
+    - Defhammer
+    - Namogod
+    - SlyTheFloof
+    - Cloud_striker
+    - Cerealbowl
+    - Beethoven
+  \`\`\`
   `;
 let m;
 
